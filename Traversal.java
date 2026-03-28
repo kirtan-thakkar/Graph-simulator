5e6686930e8d6d6f09a032c8e67293122173cda5
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-28 19:41:20 +0530

    add static void bfs()->Traversal.java

--- Traversal.java
+++ Traversal.java
@@ -1,3 +1,29 @@
+import java.util.*;
 public class Traversal {
-    
+    static void bfs(Graph graph, java.util.List<Node> nodes, GraphPanel panel) {
+        new Thread(() -> {
+            try {
+                Set<Integer> visited = new HashSet<>();
+                Queue<Integer> queue = new LinkedList<>();
+
+                queue.add(0);
+                visited.add(0);
+
+                while (!queue.isEmpty()) {
+                    int node = queue.poll();
+
+                    nodes.get(node).visited = true;
+                    panel.repaint();
+                    Thread.sleep(800);
+
+                    for (int neighbor : graph.adjList.get(node)) {
+                        if (!visited.contains(neighbor)) {
+                            visited.add(neighbor);
+                            queue.add(neighbor);
+                        }
+                    }
+                }
+            } catch (Exception ignored) {}
+        }).start();
+    }
 }
