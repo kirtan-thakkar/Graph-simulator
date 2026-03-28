1731d620a1fdba3af995ca3167a2f738c2f4eb7f
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-28 19:38:43 +0530

    Add void clear() ->Graph.java

--- Graph.java
+++ Graph.java
@@ -1,18 +1,24 @@
+
 import java.util.*;
 
 public class Graph {
-    Map<Integer, List<Integer>>adjList = new HashMap<>();
-    boolean isDirected =false;
 
-    void addNode(int id){
+    Map<Integer, List<Integer>> adjList = new HashMap<>();
+    boolean isDirected = false;
+
+    void addNode(int id) {
         adjList.putIfAbsent(id, new ArrayList<>());
     }
-    
-    void addEdge(int u , int v){
+
+    void addEdge(int u, int v) {
         adjList.get(u).add(v);
-        if(!isDirected){
+        if (!isDirected) {
             adjList.get(v).add(u);
         }
+    }
+
+    void clear() {
+        adjList.clear();
     }
 
 }
