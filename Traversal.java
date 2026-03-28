794e327df8343281c11c955d3560b60a7b662fc6
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-28 19:43:49 +0530

    add static void dfs() & dfsHelper()

--- Traversal.java
+++ Traversal.java
@@ -1,5 +1,8 @@
+
 import java.util.*;
+
 public class Traversal {
+
     static void bfs(Graph graph, java.util.List<Node> nodes, GraphPanel panel) {
         new Thread(() -> {
             try {
@@ -23,7 +26,34 @@ public class Traversal {
                         }
                     }
                 }
-            } catch (Exception ignored) {}
+            } catch (Exception ignored) {
+            }
         }).start();
     }
+
+    static void dfs(Graph graph, java.util.List<Node> nodes, GraphPanel panel) {
+        new Thread(() -> {
+            try {
+                dfsHelper(0, new HashSet<>(), graph, nodes, panel);
+            } catch (Exception ignored) {
+            }
+        }).start();
+    }
+
+    static void dfsHelper(int node, Set<Integer> visited, Graph graph,
+            java.util.List<Node> nodes, GraphPanel panel) throws InterruptedException {
+
+        visited.add(node);
+        nodes.get(node).visited = true;
+
+        panel.repaint();
+        Thread.sleep(800);
+
+        for (int neighbor : graph.adjList.get(node)) {
+            if (!visited.contains(neighbor)) {
+                dfsHelper(neighbor, visited, graph, nodes, panel);
+            }
+        }
+    }
+
 }
