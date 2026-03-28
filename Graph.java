369e63f9386f9cf5543672f83383f76805b70b77
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-28 19:36:33 +0530

    Add void addNode ->Graph.java

--- Graph.java
+++ Graph.java
@@ -3,6 +3,10 @@ import java.util.*;
 public class Graph {
     Map<Integer, List<Integer>>adjList = new HashMap<>();
     boolean isDirected =false;
+
+    void addNode(int id){
+        adjList.putIfAbsent(id, new ArrayList<>());
+    }
     
 
 }
