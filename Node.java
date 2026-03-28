e90a620b92c12441e33a87491998a78e712d328f
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-28 19:32:01 +0530

    Add node.java

--- Node.java
+++ Node.java
@@ -1,3 +1,12 @@
 public class Node {
-    
+    int id;;
+    int x;
+    int y;
+    boolean visited = true;
+
+    Node(int id, int x , int y){
+        this.id = id;
+        this.x = x;
+        this.y=y;
+    }
 }
